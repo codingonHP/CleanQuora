7283239df275f3e6b4750efbf7a3b595088c9511
Vishal Anand <vishal_24_anand@hotmail.com>
2017-06-13 10:44:19 +0530

    Added marker # so that its easy to navigate between multiple answers

--- CleanQuora/app/quora.js
+++ CleanQuora/app/quora.js
@@ -1,6 +1,6 @@
 
 function hideAllGarbage() {
-    var hideList = [
+    const hideList = [
         ".answer_user",
         ".AnswerHeader",
         ".UserUpvotedAnswerReason",
@@ -24,11 +24,45 @@ function hideAllGarbage() {
     document.querySelectorAll(hideList)
     .forEach(div => div.remove());
 
-    document.querySelectorAll('.Upvote > span.count').forEach(e => e.remove());
+    document.querySelectorAll(".Upvote > span.count").forEach(e => e.remove());
+
+    appendMarker();
+}
+
+function appendMarker() {
+
+    const dom = document.querySelectorAll(".AnswerPagedList > .pagedlist_item");
+
+    dom.forEach(div => {
+
+        if (div.firstChild && !div.firstChild.classList.contains("marker")) {
+            div.style.position = "relative";
+
+            const newNode = getMarkerDiv();
+            var firstChild = div.firstChild;
+            div.insertBefore(newNode, firstChild);
+        }
+
+    });
+}
+
+
+function getMarkerDiv() {
+
+    const newNode = document.createElement("div");
+    newNode.innerText = "#";
+    newNode.style.fontSize = "50px";
+    newNode.style.position = "absolute";
+    newNode.style.left = "-40px";
+    newNode.style.textShadow = "1px 1px";
+    newNode.className = "marker";
+
+    return newNode;
 }
 
 hideAllGarbage();
 
+
 setInterval(hideAllGarbage, 500);
 
-console.log('quora cleaner is running');
+console.log("quora cleaner is running");
